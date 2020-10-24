1b8ce564ead6de47c4162c7de48a72af51cd66b6
Igor <73028588+IgorBataljon@users.noreply.github.com>
2020-10-24 17:35:10 +0200

    Add files via upload

---
+++ AccountRuiner/AccountRuiner/CreateServer.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace AccountRuiner
+{
+    public class CreateServer
+    {
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("icon")]
+        public string icon { get; set; }
+    }
+}
---
+++ AccountRuiner/AccountRuiner/LoadImage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace AccountRuiner
+{
+    class LoadImage
+    {
+        public Image image { get; set; }
+
+        public override string ToString()
+        {
+            if (image == null)
+                return null;
+
+            string type;
+
+            if (ImageFormat.Jpeg.Equals(image.RawFormat))
+                type = "jpeg";
+            else if (ImageFormat.Png.Equals(image.RawFormat))
+                type = "png";
+            else if (ImageFormat.Gif.Equals(image.RawFormat))
+                type = "gif";
+            else
+                throw new NotSupportedException("File extension not supported");
+
+            return $"data:image/{type};base64,{Convert.ToBase64String((byte[])new ImageConverter().ConvertTo(image, typeof(byte[])))}";
+        }
+    }
+}
